ef6a4cc4dfc67572b754bcd3d44ce0c4edd307f3
JMBuccolini <juanmateobuccolini@gmail.com>
2025-09-25 11:39:41 -0300

    delete mysql

---
+++ Writing/03_delete.sql
@@ -0,0 +1,5 @@
+-- Borramos informacion de una tabla
+
+DELETE FROM users -- si hacemos esto, se borra toda la tabla de users.
+
+DELETE FROM users WHERE user_id = 7; -- borramos el usuario con id 7
